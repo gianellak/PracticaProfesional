037f62281213d2d5a33b872730e80e7b7ee4acd5
Rachel B <bronstein.rachel@gmail.com>
2016-02-07 19:04:12 -0300

    Clase vehiculo
    actualizada. falta un campo que no me acuerdo

--- src/objetos/Vehiculo.java
+++ src/objetos/Vehiculo.java
@@ -7,54 +7,19 @@ public class Vehiculo {
 	String patente;
 	String marca;
 	String modelo;
+	Integer year;
+	String color;
+	Integer km;
+	String motor;
+	String dominio;
+	Integer pvc;
 	Date fechaIngreso;
 	Date fechaVenta;
+	Integer idCondicion;
+	Integer idProveedor;
+	Integer idtitular; //cuando vende o reserva 
 	String comentarios;
-	String rachel;
-	String gianella;
 	
 	
 	
-	public Vehiculo(String patente, String marca, String modelo,
-			Date fechaIngreso, Date fechaVenta, String comentarios) {
-		super();
-		this.patente = patente;
-		this.marca = marca;
-		this.modelo = modelo;
-		this.fechaIngreso = fechaIngreso;
-		this.fechaVenta = fechaVenta;
-		this.comentarios = comentarios;
-	}
-	public void setFechaIngreso(Date fechaIngreso) {
-		this.fechaIngreso = fechaIngreso;
-	}
-	public void setFechaVenta(Date fechaVenta) {
-		this.fechaVenta = fechaVenta;
-	}
-	public String getPatente() {
-		return patente;
-	}
-	public void setPatente(String patente) {
-		this.patente = patente;
-	}
-	public String getMarca() {
-		return marca;
-	}
-	public void setMarca(String marca) {
-		this.marca = marca;
-	}
-	public String getModelo() {
-		return modelo;
-	}
-	public void setModelo(String modelo) {
-		this.modelo = modelo;
-	}
-		public String getComentarios() {
-		return comentarios;
-	}
-	public void setComentarios(String comentarios) {
-		this.comentarios = comentarios;
-	}
-	
-
 }
