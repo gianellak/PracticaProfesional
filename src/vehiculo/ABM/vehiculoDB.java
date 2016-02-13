be6273dbf0f6794a9f22b7923ef481e83c9fbe98
Rachel B <bronstein.rachel@gmail.com>
2016-02-13 19:05:56 -0300

    Vehiculo
    Nuevo Vehiculo DB

--- src/vehiculo/ABM/vehiculoDB.java
+++ src/vehiculo/ABM/vehiculoDB.java
@@ -1,22 +1,20 @@
 package vehiculo.ABM;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-import objetos.Usuario;
-import objetos.Vehiculo;
-import objetos.Venta;
+import objetos.*;
 import utilitarios.DBUtil;
 import connections.ConnectionProvider;
 import exceptions.DBException;
 
 public class vehiculoDB implements vehiculoInterface {
 
+	private static final String SQL_INSERT_VEHICULO = "INSERT INTO Vehiculo (patente, marca, modelo, year, color, km, motor, dominio, pvc, fechaIngreso, fechaVenta, idCondicion, idProveedor, idCliente, comentarios) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+	private static final int MYSQL_DUPLICATE_PK = -104;
+	
 	@SuppressWarnings("unused")
 	private ConnectionProvider connectionProvider;
 
@@ -24,9 +22,42 @@ public class vehiculoDB implements vehiculoInterface {
 		this.connectionProvider = connection;
 	}
 
-	public Vehiculo getVehiculo(int patente) {
-		// TODO Auto-generated method stub
-		return null;
+	public Vehiculo getVehiculo(String patenteInput) {
+
+		Vehiculo vehiculo = null;
+
+		try (Connection connection = this.connectionProvider.getConnection();
+				PreparedStatement statement = DBUtil.prepareStatement(
+						connection, "SELECT * FROM Vehiculo WHERE patente=?",
+						false, patenteInput);
+				ResultSet rs = statement.executeQuery();) {
+			if (rs.next()) {
+
+				Integer year = rs.getInt(4);
+				Date fechaIngreso = rs.getDate(10);
+				Date fechaVenta = rs.getDate(11);
+				Integer km = rs.getInt(6);
+				String patente = rs.getString(1);
+				String marca = rs.getString(2);
+				String modelo = rs.getString(3);
+				String color = rs.getString(5);
+				String motor = rs.getString(7);
+				String dominio = rs.getString(8);
+				Integer pvc = rs.getInt(9);
+				Integer idCondicion = rs.getInt(12);
+				Integer idProveedor = rs.getInt(13);
+				Integer idCliente = rs.getInt(14);
+				String comentarios = rs.getString(15);
+
+				vehiculo = new Vehiculo(patente, marca, modelo, year, color,
+						km, motor, dominio, pvc, fechaIngreso, fechaVenta,
+						idCondicion, idProveedor, idCliente, comentarios);
+			}
+
+		} catch (SQLException e) {
+			// throw new DBException(e);
+		}
+		return vehiculo;
 	}
 
 	public List<Vehiculo> getStock() throws DBException {
@@ -68,5 +99,42 @@ public class vehiculoDB implements vehiculoInterface {
 		}
 
 		return stock;
+	}
+
+	@Override
+	public Boolean createVehiculo(Vehiculo vehiculo) throws DBException {
+		Object[] values = { 
+				vehiculo.getPatente(),
+				vehiculo.getMarca(),
+				vehiculo.getModelo(),
+				vehiculo.getYear(),
+				vehiculo.getColor(),
+				vehiculo.getKm(),
+				vehiculo.getMotor(),
+				vehiculo.getDominio(),
+				vehiculo.getPvc(),
+				vehiculo.getFechaIngreso(),
+				vehiculo.getFechaVenta(),
+				vehiculo.getIdCondicion(),
+				vehiculo.getIdProveedor(),
+				vehiculo.getIdCliente(),
+				vehiculo.getComentarios()
+				};
+		
+		try (
+	            Connection connection = this.connectionProvider.getConnection();
+	            PreparedStatement statement = DBUtil.prepareStatement(connection, SQL_INSERT_VEHICULO, true, values);
+	        ) 
+	            
+	        	{
+		        	 statement.executeUpdate();
+		       } catch (SQLException e) {
+		    	   if(e.getErrorCode() == MYSQL_DUPLICATE_PK){
+		    		   throw new DBException("Duplicated Key, cannot insert user");
+		    	    }else{
+	            throw new DBException(e);}
+	        }
+
+		return null;
 	}
 }
--- src/vehiculo/ABM/vehiculoInterface.java
+++ src/vehiculo/ABM/vehiculoInterface.java
@@ -7,7 +7,8 @@ import objetos.Vehiculo;
 
 public interface vehiculoInterface {
 	
-	Vehiculo getVehiculo(int patente);
+	Vehiculo getVehiculo(String patente);
 	List<Vehiculo> getStock() throws DBException;
+	Boolean createVehiculo(Vehiculo vehiculo) throws DBException;
 
 }
